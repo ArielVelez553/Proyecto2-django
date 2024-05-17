bdd57454ec86875616ea5bb4468c85f7d4e39958
ArielVelez553 <arielvelez552@gmail.com>
2024-05-17 18:30:43 -0500

    first commit

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/us/bin/env bash
+#!/usr/bin/env bash
 
 # exit on error
 set -o errexit
@@ -8,3 +8,6 @@ pip istall -r requirements.txt
 python manage.py collectstatic --no-input
 python manage-py migrate
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
